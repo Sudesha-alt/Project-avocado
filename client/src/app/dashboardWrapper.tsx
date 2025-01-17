37124c5a4530815779256e61f0f09f478b891991
Sudesha-alt <sudesha.tr@convin.ai>
2025-01-18 01:34:31 +0530

    Sidebar and Navbar created

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 const dashboardWrapper = ({children}: {children: React.ReactNode}) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-        sidebar
-        <main className="flex flex-col bg-gray-50 dark:bg-dark-bg">
-            navbar
+        <Sidebar />
+        <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}>
+            <Navbar />
             {children}
         </main>
     </div>
