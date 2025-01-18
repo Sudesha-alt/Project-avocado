a35e93dd1b8128c48de37174760cebf6c8122712
Sudesha-alt <sudesha.tr@convin.ai>
2025-01-18 15:50:07 +0530

    Made Sidebar and navbar. Added features like theme change to the navbar.

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,16 +1,41 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import StoreProvider, { useAppSelector } from './components/redux';
 
-const dashboardWrapper = ({children}: {children: React.ReactNode}) => {
+const DashboardLayout = ({children}: {children: React.ReactNode}) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
         <Sidebar />
-        <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}>
+        <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 ${isSidebarCollapsed ? "" : "md:pl-64"}`}>
             <Navbar />
             {children}
         </main>
     </div>
   )
 }
-export default dashboardWrapper
+
+const DashboardWrapper = ({children}: {children: React.ReactNode}) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
+export default DashboardWrapper;
