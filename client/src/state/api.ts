3afc3295fd842f0d9bf9f5891fe8d4743aca1b2c
Sudesha-alt <sudesha.tr@convin.ai>
2025-01-28 22:37:54 +0530

    Changes made

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,64 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export interface Project{
-    id: number;
-    name: string;
-    description?: string;
-    startDate?: string;
-    endDate?: string;
-}
-
-export enum Status{
-    ToDo = "To Do",
-    WorkInProgress = "Work In Progress",
-    UnderReview = "Under Review",
-    Completed = "Completed"
-}
-
-export enum Priority{
-     Urgent = "Urgent",
-     HIgh = "High",
-     Medium = "Medium",
-     Low = "Low",
-     Backlog = "Backlog"
-}
-
-export interface User{
-    userId?: number;
-    username: string;
-    email: string;
-    profilePictureUrl?: string;
-    cognitoId?: string;
-    teamId?: number;
-}
-
-export interface Attachment{
-    id: number;
-    fileUrl: string;
-    fileName: string;
-    taskId: number;
-    uploadedById: number;
-}
-export interface Task{
-    id: number;
-    title: string;
-    description?: string;
-    status?: Status;
-    priority?: Priority;
-    tags?: string;
-    startDate?: string;
-    dueDate?: string;
-    points?: number;
-    projectId?: number;
-    authorUserId?: number;
-    assignedUserId?: number;
-
-    author?: User;
-    assignee?: User;
-    comments?: Comment[];
-    attachments?: Attachment[];
-}
-
 
 export interface Project{
     id: number;
@@ -68,20 +9,6 @@ export interface Project{
     endDate?: string;
 }
 
-export enum Status{
-    ToDo = "To Do",
-    WorkInProgress = "Work In Progress",
-    UnderReview = "Under Review",
-    Completed = "Completed"
-}
-
-export enum Priority{
-     Urgent = "Urgent",
-     HIgh = "High",
-     Medium = "Medium",
-     Low = "Low",
-     Backlog = "Backlog"
-}
 
 export interface User{
     userId?: number;
@@ -121,65 +48,6 @@ export interface Task{
 
 
 
-export interface Project {
-    id: number;
-    name: string;
-    description?: string;
-    startDate?: string;
-    endDate?: string;
-  }
-  
-  export enum Priority {
-    Urgent = "Urgent",
-    High = "High",
-    Medium = "Medium",
-    Low = "Low",
-    Backlog = "Backlog",
-  }
-  
-  export enum Status {
-    ToDo = "To Do",
-    WorkInProgress = "Work In Progress",
-    UnderReview = "Under Review",
-    Completed = "Completed",
-  }
-
-  export interface User {
-    userId?: number;
-    username: string;
-    email: string;
-    profilePictureUrl?: string;
-    cognitoId?: string;
-    teamId?: number;
-  }
-
-  export interface Attachment {
-    id: number;
-    fileURL: string;
-    fileName: string;
-    taskId: number;
-    uploadedById: number;
-  }
-
-export interface Task {
-    id: number;
-    title: string;
-    description?: string;
-    status?: Status;
-    priority?: Priority;
-    tags?: string;
-    startDate?: string;
-    dueDate?: string;
-    points?: number;
-    projectId: number;
-    authorUserId?: number;
-    assignedUserId?: number;
-
-    author?: User;
-    assignee?: User;
-    comments?: Comment[];
-    attachments?: Attachment[];
-  }
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
     reducerPath: "api",
